59519c068d9daeac74e8022c80b133e7387a06fc
Alix Lourme <alix.lourme@gmail.com>
2019-06-11 16:03:12 +0200

    Fix #8 : System environment variables & password prompt support

--- src/main/java/org/psa/sonarqube/cleanup/config/Config.java
+++ src/main/java/org/psa/sonarqube/cleanup/config/Config.java
@@ -1,13 +1,20 @@
 package org.psa.sonarqube.cleanup.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.lang3.StringUtils;
 
 public class Config {
+
+    public static final int USER_TOKEN_LENGTH_MIN = 40;
 
     private static final String OPTION_HOST_URL = "h";
     private static final String OPTION_LOGIN = "l";
@@ -16,6 +23,22 @@ public class Config {
     private static final String OPTION_YES = "y";
     private static final String OPTION_THRESHOLD_COEFF = "t";
     private static final String OPTION_NUMBER_LOC_ADD = "n";
+
+    private static final String SYSENV_HOST_URL = "SONAR_HOST_URL";
+    private static final String SYSENV_LOGIN = "SONAR_LOGIN";
+    private static final String SYSENV_PASSWORD = "SONAR_PASSWORD"; // NOSONAR : Key reference
+    private static final String SYSENV_DRY_RUN = "SONAR_CLEANUP_DRY_RUN";
+    private static final String SYSENV_YES = "SONAR_CLEANUP_YES";
+    private static final String SYSENV_THRESHOLD_COEFF = "SONAR_CLEANUP_THRESHOLD_COEFF";
+    private static final String SYSENV_NUMBER_LOC_ADD = "SONAR_CLEANUP_NUMBER_LOC_ADD";
+
+    private static final String DESC_HOST_URL = "The SonarQube server URL (or sysenv '%s').";
+    private static final String DESC_LOGIN = "The login or authentication token of a SonarQube user with admin permission (or sysenv '%s').";
+    private static final String DESC_PASSWORD = "The password that goes with the login username. This should be left blank if an authentication token is being used (or sysenv '%s')."; // NOSONAR
+    private static final String DESC_DRY_RUN = "Dry run mode, no projects will be deleted (default: false ; or sysenv '%s').";
+    private static final String DESC_YES = "Assume projects deletion without confirmation (default: false ; or sysenv '%s').";
+    private static final String DESC_THRESHOLD_COEFF = "The multiplicative coefficient on threshold to calculate LoC to retrieve (default: 2 ; or sysenv '%s').";
+    private static final String DESC_NUMBER_LOC_ADD = "The number of LoC in addition to retrieve (default: 0 ; or sysenv '%s').";
 
     private String hostUrl;
     private String login;
@@ -26,7 +49,7 @@ public class Config {
     private long numberLocAdd = 0;
 
     public Config(String[] args) {
-        CommandLine cmd = parseCmd(args);
+        CommandLine cmd = parseCmd(populateWithSysEnv(args));
         this.hostUrl = cmd.getOptionValue(OPTION_HOST_URL);
         this.login = cmd.getOptionValue(OPTION_LOGIN);
         this.password = cmd.getOptionValue(OPTION_PASSWORD);
@@ -38,19 +61,22 @@ public class Config {
         if (cmd.hasOption(OPTION_NUMBER_LOC_ADD)) {
             this.numberLocAdd = Long.parseLong(cmd.getOptionValue(OPTION_NUMBER_LOC_ADD));
         }
+
+        // Minimal checks in addition of parsing
+        if (isLoginUserToken() && StringUtils.isNotBlank(getPassword())) {
+            throw new UnsupportedOperationException("User token is used for connection and password is not empty, invalid configuration");
+        }
     }
 
     private static CommandLine parseCmd(String[] args) {
         Options options = new Options();
-        options.addRequiredOption(OPTION_HOST_URL, "hostUrl", true, "The SonarQube server URL.");
-        options.addRequiredOption(OPTION_LOGIN, "login", true, "The login or authentication token of a SonarQube user with admin permission.");
-        options.addOption(OPTION_PASSWORD, "password", true,
-                "The password that goes with the login username. This should be left blank if an authentication token is being used.");
-        options.addOption(OPTION_DRY_RUN, "dryRun", false, "Dry run mode, no projects will be deleted (default: false).");
-        options.addOption(OPTION_YES, "yes", false, "Assume projects deletion without confirmation (default: false).");
-        options.addOption(OPTION_THRESHOLD_COEFF, "thresholdCoeff", true,
-                "The multiplicative coefficient on threshold to calculate LoC to retrieve (default: 2).");
-        options.addOption(OPTION_NUMBER_LOC_ADD, "numberLocAdd", true, "The number of LoC in addition to retrieve (default: 0).");
+        options.addRequiredOption(OPTION_HOST_URL, "hostUrl", true, String.format(DESC_HOST_URL, SYSENV_HOST_URL));
+        options.addRequiredOption(OPTION_LOGIN, "login", true, String.format(DESC_LOGIN, SYSENV_LOGIN));
+        options.addOption(OPTION_PASSWORD, "password", true, String.format(DESC_PASSWORD, SYSENV_PASSWORD));
+        options.addOption(OPTION_DRY_RUN, "dryRun", false, String.format(DESC_DRY_RUN, SYSENV_DRY_RUN));
+        options.addOption(OPTION_YES, "yes", false, String.format(DESC_YES, SYSENV_YES));
+        options.addOption(OPTION_THRESHOLD_COEFF, "thresholdCoeff", true, String.format(DESC_THRESHOLD_COEFF, SYSENV_THRESHOLD_COEFF));
+        options.addOption(OPTION_NUMBER_LOC_ADD, "numberLocAdd", true, String.format(DESC_NUMBER_LOC_ADD, SYSENV_NUMBER_LOC_ADD));
         CommandLineParser parser = new DefaultParser();
         try {
             return parser.parse(options, args);
@@ -61,6 +87,29 @@ public class Config {
         }
     }
 
+    private static String[] populateWithSysEnv(String[] args) {
+        args = parseSysEnvOverride(args, SYSENV_HOST_URL, OPTION_HOST_URL);
+        args = parseSysEnvOverride(args, SYSENV_LOGIN, OPTION_LOGIN);
+        args = parseSysEnvOverride(args, SYSENV_PASSWORD, OPTION_PASSWORD);
+        args = parseSysEnvOverride(args, SYSENV_DRY_RUN, OPTION_DRY_RUN);
+        args = parseSysEnvOverride(args, SYSENV_YES, OPTION_YES);
+        args = parseSysEnvOverride(args, SYSENV_THRESHOLD_COEFF, OPTION_THRESHOLD_COEFF);
+        args = parseSysEnvOverride(args, SYSENV_NUMBER_LOC_ADD, OPTION_NUMBER_LOC_ADD);
+        return args;
+    }
+
+    private static String[] parseSysEnvOverride(String[] args, String sysEnv, String optionName) {
+        String value = System.getenv(sysEnv);
+        if (StringUtils.isBlank(value)) {
+            return args;
+        }
+        List<String> l = new ArrayList<>();
+        l.add("-" + optionName);
+        l.add(value);
+        l.addAll(Arrays.asList(args));
+        return l.toArray(new String[l.size()]);
+    }
+
     public String getHostUrl() {
         return hostUrl;
     }
@@ -69,8 +118,23 @@ public class Config {
         return login;
     }
 
+    public String getLoginForDisplay() {
+        if (isLoginUserToken()) {
+            return "[tokenHidden]";
+        }
+        return getLogin();
+    }
+
+    public boolean isLoginUserToken() {
+        return getLogin().length() >= USER_TOKEN_LENGTH_MIN;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public boolean isDryRun() {
--- src/test/java/org/psa/sonarqube/cleanup/config/ConfigTest.java
+++ src/test/java/org/psa/sonarqube/cleanup/config/ConfigTest.java
@@ -1,11 +1,17 @@
 package org.psa.sonarqube.cleanup.config;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
 
 public class ConfigTest {
 
     private static final String HOST_LOCAL = "http://localhost";
+    private static final String HOST_COMPANY = "http://sonar.company.com";
+
+    @Rule
+    public final EnvironmentVariables sysEnv = new EnvironmentVariables();
 
     @Test
     public void testCmdParserShortMandatoryWithDefaults() {
@@ -17,6 +23,8 @@ public class ConfigTest {
         Assert.assertEquals(false, config.isYes());
         Assert.assertEquals(2, config.getThresholdCoeff());
         Assert.assertEquals(0, config.getNumberLocAdd());
+        Assert.assertEquals(config.getLogin(), config.getLoginForDisplay());
+        Assert.assertFalse(config.isLoginUserToken());
     }
 
     @Test
@@ -41,6 +49,45 @@ public class ConfigTest {
         Assert.assertEquals(true, config.isYes());
         Assert.assertEquals(1, config.getThresholdCoeff());
         Assert.assertEquals(2, config.getNumberLocAdd());
+    }
+
+    @Test
+    public void testCmdParserUserToken() {
+        Config config = new Config(new String[] { "-h", HOST_LOCAL, "-l", "0000000000000000000000000000000000000000" });
+        Assert.assertEquals("[tokenHidden]", config.getLoginForDisplay());
+        Assert.assertTrue(config.isLoginUserToken());
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testCmdParserUserTokenAndPassword() {
+        new Config(new String[] { "-h", HOST_LOCAL, "-l", "0000000000000000000000000000000000000000", "-p", "somePass" });
+    }
+
+    @Test
+    public void testCmdParserSysEnvComplete() {
+        sysEnv.set("SONAR_HOST_URL", HOST_COMPANY);
+        sysEnv.set("SONAR_LOGIN", "admin");
+        sysEnv.set("SONAR_PASSWORD", "password");
+        sysEnv.set("SONAR_CLEANUP_DRY_RUN", "true");
+        sysEnv.set("SONAR_CLEANUP_YES", "true");
+        sysEnv.set("SONAR_CLEANUP_THRESHOLD_COEFF", "4");
+        sysEnv.set("SONAR_CLEANUP_NUMBER_LOC_ADD", "5");
+
+        Config config = new Config(new String[] { "-h", HOST_LOCAL, "-l", "foo", "-p", "bar", "-t", "1", "-n", "2" });
+        Assert.assertEquals(HOST_COMPANY, config.getHostUrl());
+        Assert.assertEquals("admin", config.getLogin());
+        Assert.assertEquals("password", config.getPassword());
+        Assert.assertEquals(true, config.isDryRun());
+        Assert.assertEquals(true, config.isYes());
+        Assert.assertEquals(4, config.getThresholdCoeff());
+        Assert.assertEquals(5, config.getNumberLocAdd());
+    }
+
+    @Test
+    public void testCmdParserSysEnvSomeEmpty() {
+        sysEnv.set("SONAR_HOST_URL", "");
+        Config config = new Config(new String[] { "-h", HOST_LOCAL, "-l", "foo" });
+        Assert.assertEquals(HOST_LOCAL, config.getHostUrl());
     }
 
     @Test
